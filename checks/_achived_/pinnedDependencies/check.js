e5baf9a18c9f401e6ff4438b5e9d6e11a2a95bc6
Martin M. <mcm57@gmx.at>
2025-09-12 11:07:53 +0200

    archive pinnedDependencies

--- checks/_achived_/pinnedDependencies/check.js
+++ checks/_achived_/pinnedDependencies/check.js
@@ -6,9 +6,8 @@ const iobroker = require('../../../lib/iobrokerTools.js');
 const context = {};
 
 async function doCheck(repoUrl) {
-
-    await github.init( repoUrl );
-    context.package = await github.downloadFile('/package.json', false, false);    
+    await github.init(repoUrl);
+    context.package = await github.downloadFile('/package.json', false, false);
     //console.log( JSON.stringify(context.package));
 
     console.log('starting check');
@@ -16,34 +15,49 @@ async function doCheck(repoUrl) {
     const pinnedDependencies = [];
     for (const dependency in context.package.dependencies) {
         const versString = context.package.dependencies[dependency];
-        if ( dependency != '@iobroker/adapter-core') continue ;
-        if ( versString.startsWith('^') || versString.startsWith('~') ) continue;
-        if ( versString.startsWith('>') ) continue;
-        if ( versString === '*') continue;
-        
-        console.log(`pinned dependency "${dependency}":"${context.package.dependencies[dependency]}"`)
+        if (dependency != '@iobroker/adapter-core') {
+            continue;
+        }
+        if (versString.startsWith('^') || versString.startsWith('~')) {
+            continue;
+        }
+        if (versString.startsWith('>')) {
+            continue;
+        }
+        if (versString === '*') {
+            continue;
+        }
+
+        console.log(`pinned dependency "${dependency}":"${context.package.dependencies[dependency]}"`);
         pinnedDependencies.push(`"${dependency}":"${context.package.dependencies[dependency]}"`);
     }
-    
+
     for (const dependency in context.package.devDependencies) {
         const versString = context.package.devDependencies[dependency];
-        if ( dependency != '@iobroker/adapter-core') continue ;
-        if ( versString.startsWith('^') || versString.startsWith('~') ) continue;
-        if ( versString.startsWith('>') ) continue;
-        if ( versString === '*') continue;
-        
-        console.log(`pinned dependency "${dependency}":"${context.package.devDependencies[dependency]}"`)
+        if (dependency != '@iobroker/adapter-core') {
+            continue;
+        }
+        if (versString.startsWith('^') || versString.startsWith('~')) {
+            continue;
+        }
+        if (versString.startsWith('>')) {
+            continue;
+        }
+        if (versString === '*') {
+            continue;
+        }
+
+        console.log(`pinned dependency "${dependency}":"${context.package.devDependencies[dependency]}"`);
         pinnedDependencies.push(`"${dependency}":"${context.package.devDependencies[dependency]}"`);
     }
 
     if (pinnedDependencies.length) {
-        console.log( 'check FAILED, pinned dependencies detected')
+        console.log('check FAILED, pinned dependencies detected');
     } else {
-        console.log( 'check PASSED, no pinned dependencies detected')
+        console.log('check PASSED, no pinned dependencies detected');
     }
 
     return pinnedDependencies;
-
 }
 
 async function exec() {
@@ -51,22 +65,22 @@ async function exec() {
 
     // Get url from parameters if possible
     if (process.argv.length > 2) {
-        repoUrls = [ process.argv[2] ];
+        repoUrls = [process.argv[2]];
     } else {
         repoUrls = await iobroker.getAdapterUrls();
     }
 
     const report = [];
     for (let repoUrl of repoUrls) {
-        console.log( `Checking repository ${repoUrl}` );
+        console.log(`Checking repository ${repoUrl}`);
         const pinnedDependencies = await doCheck(repoUrl);
         if (pinnedDependencies.length) {
-            const adapterName=iobroker.getAdapterName(repoUrl);
+            const adapterName = iobroker.getAdapterName(repoUrl);
             report.push(`${adapterName}`);
             for (let pinnedDependency of pinnedDependencies) {
-                report.push('    ' + pinnedDependency);
+                report.push(`    ${pinnedDependency}`);
             }
-            report.push ('');
+            report.push('');
         }
     }
 
@@ -74,7 +88,6 @@ async function exec() {
         console.log(line);
     }
     return;
-};
+}
 
 exec();
-
